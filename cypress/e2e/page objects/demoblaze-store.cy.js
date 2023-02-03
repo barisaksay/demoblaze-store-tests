17d439a7265a968dd999a7d4695489f9cfb97a9a
baris_aksay <barish.aksay@gmail.com>
2023-02-03 21:41:36 +0100

    page objects folder and relevant methods are introduced. Few more test scripts are added.

---
+++ cypress/e2e/page objects/demoblaze-store.cy.js
@@ -0,0 +1,54 @@
+export class StorePage {
+  //navigates to store homepage
+  navigate() {
+    cy.visit("https://www.demoblaze.com/index.html");
+    cy.get("#cat").should("have.text", "CATEGORIES");
+  }
+
+  //logins with correct credentials
+  login(username, password) {
+    cy.get("[class='nav-link']").contains("Log in").click();
+    cy.wait(1000);
+    cy.get("[id='loginusername']").type(username);
+    cy.get("[id='loginpassword']").type(password);
+    cy.get(".modal-footer button[onclick='logIn()']").click();
+    //below is verification:  should this line be taken out of the prototype and used in the test
+    //file directly for better usability of the prototype ?
+    cy.get("a[data-target='#signInModal']")
+      .contains("Sign up")
+      .should("not.be.visible");
+  }
+
+  //logs out a logged in user
+  logout() {
+    cy.get("a[onclick='logOut()']").contains("Log out").click();
+
+    //verification below
+    cy.get("a[onclick='logOut()']")
+      .contains("Log out")
+      .should("not.be.visible");
+  }
+
+  //clicks on an item
+  clickOnItem(itemIndex){
+    cy.get(".card > a").eq(itemIndex).click();
+    cy.contains("Add to cart").should("have.text","Add to cart")
+
+  }
+
+  //add item to cart
+  addToCart(){
+    cy.get("a").contains("Add to cart").click()
+  }
+
+  //select category
+  selectCategory(categoryName){
+    cy.get("a.list-group-item").contains(categoryName).click();
+    //verification: takes the name of the category entered,converts to lowercase and removes the last char
+    //Since <p> tag ought to contain the name of the category but may not in plural form.
+    categoryName = categoryName.toLowerCase().substring(0,categoryName.length-1)
+    //there are multiple elements with .card-text class. Rather than changing the locator,
+    //eq is chained and located the first element with .card-text and verified it contains the name entered.
+    cy.get(".card-text").eq(0).should("contain.text",categoryName)
+  }
+}
--- cypress/e2e/tests/demoblaze-store.cy.js
+++ cypress/e2e/tests/demoblaze-store.cy.js
@@ -1,33 +1,64 @@
 /// <reference types="cypress"/>
 
-describe("demoblaze store test suite",()=>{
-    it("should go to home page",()=>{
-        cy.visit("https://www.demoblaze.com/index.html")
-        cy.get("#cat").should("have.text","CATEGORIES")
-    })
+import { StorePage } from "../page objects/demoblaze-store.cy";
 
-    it("should login",()=>{
-        cy.visit("https://www.demoblaze.com/index.html")
-        cy.get("#cat").should("have.text","CATEGORIES")
-        cy.get("[class='nav-link']").contains("Log in").click()
-        cy.wait(1000)
-        cy.get("[id='loginusername']").type("QA-test")
-        cy.get("[id='loginpassword']").type("!12BN")
-        cy.get(".modal-footer button[onclick='logIn()']").click()
-        cy.get("a[data-target='#signInModal']").contains("Sign up").should("not.be.visible")
-    })
+describe("demoblaze store test suite", () => {
+  const storePage = new StorePage();
+  
+  const username = "QA-test";
+  const password = "!12BN";
 
-    it("should be able to send contact message",()=>{
-        cy.visit("https://www.demoblaze.com/index.html")
-        cy.get("#cat").should("have.text","CATEGORIES")
-        cy.get("a[class='nav-link']").contains("Contact").click()
-        cy.wait(1000)
+  it("should go to home page", () => {
+    storePage.navigate();
+  });
 
-        cy.get("#recipient-email").type("test-user@email.com")
-        cy.get("#recipient-name").type("test-name")
-        cy.get("#message-text").type("test-msg")
-        cy.get("button").contains("Send message").click()
+  it("should login", () => {
+    storePage.navigate();
+    storePage.login(username, password);
+  });
 
+  it("should log out a logged in user", () => {
+    storePage.navigate();
+    storePage.login(username, password);
+    //wait command is used just in order to wait for page to reload after the login action.
+    //Therefore acting as a prevention against possibılty of failing the logOut action due to not being
+    //able to find the LogOut button.
+    cy.wait(2000)
+    storePage.logout();
+  });
 
-    })
-})
+  it("should be able to send contact message", () => {
+    cy.visit("https://www.demoblaze.com/index.html");
+    cy.get("#cat").should("have.text", "CATEGORIES");
+    cy.get("a[class='nav-link']").contains("Contact").click();
+    cy.wait(1000);
+
+    cy.get("#recipient-email").type("test-user@email.com");
+    cy.get("#recipient-name").type("test-name");
+    cy.get("#message-text").type("test-msg");
+    cy.get("button").contains("Send message").click();
+  });
+
+  it("should go to cart", () => {
+    cy.visit("https://www.demoblaze.com/index.html");
+    cy.get("#cat").should("have.text", "CATEGORIES");
+    cy.contains("Cart").click();
+    cy.get("h2").contains("Products").should("be.visible");
+  });
+
+  it("should click on a product", () => {
+    storePage.navigate();
+    storePage.clickOnItem(1);
+  });
+
+  it("should add the selected item to the cart",()=>{
+    storePage.navigate()
+    storePage.clickOnItem(1)
+    storePage.addToCart()
+  });
+
+  it("should change category",()=>{
+    storePage.navigate()
+    storePage.selectCategory("Laptops")
+  })
+});
